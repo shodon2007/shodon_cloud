0003639ff9023272be656eb8d2c6909cc33f8d1f
shodon <about@shodon.ru>
2024-02-17 14:01:09 +0700

    update code

--- client/src/components/FileExlorer/FileExplorer.tsx
+++ client/src/components/FileExlorer/FileExplorer.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import api, { Item } from '../../api/api';
 import cls from './FileExplorer.module.scss';
+import FileImg from '../../static/file.png';
 
 const checkTypes = ['.png', '.jpeg', '.webp', '.jpg'];
 
@@ -49,10 +50,15 @@ const FileExplorer = () => {
                             <img className={cls.img} src={`${api.url}/uploads/${el.title}`} alt="картинка" />
                             <div className={cls.title}>{truncate(el.title)}</div>
                         </div>
+                    } else if (el.type === 'folder') {
+                        return <div key={index}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px"><path fill="#ffa000" d="M40,12H22l-4-4H8c-2.2,0-4,1.8-4,4v24c0,2.2,1.8,4,4,4h29.7L44,29V16C44,13.8,42.2,12,40,12z" /><path fill="#ffca28" d="M40,12H8c-2.2,0-4,1.8-4,4v20c0,2.2,1.8,4,4,4h32c2.2,0,4-1.8,4-4V16C44,13.8,42.2,12,40,12z" /></svg>
+                            <div className={cls.title}>папка {truncate(el.title)}</div>
+                        </div>
                     } else {
                         return <div key={index}>
-                            <path className={cls.img} d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"></path>
-                            <div className={cls.title}>{truncate(el.title)}</div>
+                            <img className={cls.img} src={FileImg} />
+                            <div className={cls.title}>другой файл{truncate(el.title)}</div>
                         </div>
                     }
                 })
